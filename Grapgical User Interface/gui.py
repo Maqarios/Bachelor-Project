628a6f29274ef2c749197d775af5a4dc07621873
Maqarios <maqarioswagdy@gmail.com>
2019-05-06 20:20:38 +0200

    feat: add record using signal-by-signal

--- Grapgical User Interface/gui.py
+++ Grapgical User Interface/gui.py
@@ -2,6 +2,7 @@ import numpy
 import tkinter
 import random
 import socket
+import math
 from preprocessor import P300_Preprocessor
 import matplotlib
 
@@ -24,11 +25,7 @@ class P300_GUI(object):
             font = 'Courier 10',
             bg = 'black',
             fg = 'grey',
-            color_array = numpy.array(['red', 'green', 'blue']),
-            start_delay = 2500,
-            state_0_delay = 100,
-            state_1_delay = 75,
-            end_delay = 2000
+            color_array = numpy.array(['red', 'green', 'blue'])
         ):
         
         # Instance Variables
@@ -38,12 +35,7 @@ class P300_GUI(object):
         self.bg = bg
         self.fg = fg
         self.color_array = color_array
-        self.start_delay = start_delay
-        self.state_0_delay = state_0_delay
-        self.state_1_delay = state_1_delay
-        self.end_delay = end_delay
         
-        self.is_session = False
         self.stimulus_code = -1
         
         # Tkinter Initialization
@@ -80,9 +72,6 @@ class P300_GUI(object):
     # Session Run
     def start_session(self):
         
-        # Set Session State To True
-        self.is_session = True
-        
         # Intensification Order Initialization
         self.intensification_order = numpy.arange(self.character_matrix.shape[0] + self.character_matrix.shape[1])
         numpy.random.shuffle(self.intensification_order)
@@ -90,30 +79,11 @@ class P300_GUI(object):
         # Chosen Intensification Initialization
         self.intensification_order_index = 0
         self.stimulus_code = -1
-        
-        # Sleep for 2.5s Then Start
-        self.root.after(self.start_delay, self.state_0)
-    
-    def repeat_session(self):
-        
-        # Intensification Order Initialization
-        self.intensification_order = numpy.arange(self.character_matrix.shape[0] + self.character_matrix.shape[1])
-        numpy.random.shuffle(self.intensification_order)
-        
-        # Chosen Intensification Initialization
-        self.intensification_order_index = 0
-        self.stimulus_code = -1
-        
-        # Start Immediately
-        self.state_0()
-    
-    # Session End
-    def end_session(self):
-        self.is_session = False
         
     # State Where Row / Column Is Intensified For 100ms
     def state_0(self):
         
+        # Reset Colors
         self.reset_colors()
         
         # Get Chosen Row / Column
@@ -126,9 +96,6 @@ class P300_GUI(object):
         color_index = random.randint(0, self.color_array.shape[0] - 1)
         for label in row_column_labels:
             label.config(fg = self.color_array[color_index])
-        
-        # Wait Then Move To Next State
-        self.root.after(self.state_0_delay, self.state_1)
     
     # State Where No Row / Column Is Intensified For 75ms
     def state_1(self):
@@ -138,13 +105,6 @@ class P300_GUI(object):
         
         # Check If Final Index
         self.intensification_order_index += 1
-        if self.intensification_order_index < self.intensification_order.shape[0]:
-            self.root.after(self.state_1_delay, self.state_0)
-        elif self.controller.temp_repetitions != 0:
-            self.controller.temp_repetitions -= 1
-            self.root.after(self.state_1_delay, self.repeat_session)
-        else:
-            self.root.after(self.end_delay, self.end_session)
 # -------------------------------------------- ### -------------------------------------------- #
 
 
@@ -154,7 +114,7 @@ class P300_GUI(object):
 # -------------------------------------------- Socket -------------------------------------------- #
 class P300_Socket(object):
     
-    def __init__(self, controller, HOST, PORT, BUFFER_SIZE = 256, FLUSH_SIZE = 300):
+    def __init__(self, controller, HOST, PORT, BUFFER_SIZE = 1024, FLUSH_SIZE = 300):
         
         self.controller = controller
         self.HOST = HOST
@@ -206,20 +166,20 @@ class P300_Controller(object):
             channels = 14,
             start_sample = 410,
             end_sample = 910,
+            samples_per_seconds = 128,
+            start_delay = 2.5,
+            state_0_delay = 0.6,
+            state_1_delay = 0.2,
+            end_delay = 1.0,
             
             bg = 'black',
             fg = 'grey',
             color_array = numpy.array(['red', 'green', 'blue']),
             font = 'Courier 10',
-            start_delay = 2500,
-            state_0_delay = 100,
-            state_1_delay = 75,
-            end_delay = 2000,
-            
             
             IP = '127.0.0.1',
             PORT = 54123,
-            BUFFER_SIZE = 256
+            BUFFER_SIZE = 1024
         ):
         
         self.gui = P300_GUI(
@@ -228,11 +188,7 @@ class P300_Controller(object):
                 font = font,
                 bg = bg,
                 fg = fg,
-                color_array = color_array,
-                start_delay = start_delay,
-                state_0_delay = state_0_delay,
-                state_1_delay = state_1_delay,
-                end_delay = end_delay
+                color_array = color_array
             )
         
         self.socket = P300_Socket(
@@ -246,6 +202,16 @@ class P300_Controller(object):
         self.channels = channels
         self.start_sample = start_sample
         self.end_sample = end_sample
+        self.samples_per_seconds = samples_per_seconds
+        self.start_delay = start_delay
+        self.state_0_delay = state_0_delay
+        self.state_1_delay = state_1_delay
+        self.end_delay = end_delay
+        
+        self.start_samples_count = math.floor(self.start_delay * self.samples_per_seconds)
+        self.state_0_samples_count = math.floor(self.state_0_delay * self.samples_per_seconds)
+        self.state_1_samples_count = math.floor(self.state_1_delay * self.samples_per_seconds)
+        self.end_samples_count = math.floor(self.end_delay * self.samples_per_seconds)
         
         self.temp_repetitions = 0
     
@@ -290,7 +256,9 @@ class P300_Controller(object):
                         moving_average=0,
                         digitization_samples=128,
                         end_window=100
-                    ).plot()
+                    ).plot(
+                        average_channels=True
+                    )
         
         return signal_session, stimulus_code_session
     
@@ -329,17 +297,16 @@ class P300_Controller(object):
     def record(self):
          
         # Clear Buffer
-        #self.socket.flush()
+        self.socket.flush()
         
         # Variable Initialization
         signal = numpy.empty((0, self.channels))
         stimulus_code = numpy.empty((0))
         self.temp_repetitions = self.repetitions - 1
         
-        # Start GUI Session
-        self.gui.start_session()
-        
+        # Start Delay
         message = ''
+        message_counter = 0
         while 1:
             
             # Update GUI
@@ -362,9 +329,83 @@ class P300_Controller(object):
                     message = ''
                 else:
                     message = temp_message[1]
+                
+                message_counter += 1
+                if message_counter == self.start_samples_count:
+                    break
+        
+        # Start GUI Session
+        self.gui.start_session()
+        self.gui.state_0()
+        
+        message = ''
+        message_counter = 0
+        while 1:
             
-            if self.gui.is_session == False:
-                break
+            # Update GUI
+            self.gui.update()
+            
+            # Add Message To Previous Message
+            message += self.socket.receive().decode('utf-8')
+            
+            # Split Message & Read First Part
+            temp_message = message.split('\r\n')
+            data = numpy.fromstring(temp_message[0], dtype = numpy.float, sep=',')[2 : 2 + self.channels]
+            
+            if data.shape[0] == self.channels:
+                
+                # Save Data
+                signal = numpy.append(signal, data.reshape(1, self.channels), axis = 0)
+                stimulus_code = numpy.append(stimulus_code, self.gui.stimulus_code + 1)
+                
+                if len(temp_message) == 1:
+                    message = ''
+                else:
+                    message = temp_message[1]
+                
+                message_counter += 1
+                if message_counter == self.state_0_samples_count:
+                    self.gui.state_1()
+                elif message_counter == self.state_0_samples_count + self.state_1_samples_count:
+                    message_counter = 0
+                    if self.gui.intensification_order_index < self.gui.intensification_order.shape[0]:
+                        self.gui.state_0()
+                    elif self.temp_repetitions != 0:
+                        self.temp_repetitions -= 1
+                        self.gui.start_session()
+                        self.gui.state_0()
+                    else:
+                        break
+        
+        # Start Delay
+        message = ''
+        message_counter = 0
+        while 1:
+            
+            # Update GUI
+            self.gui.update()
+            
+            # Add Message To Previous Message
+            message += self.socket.receive().decode('utf-8')
+            
+            # Split Message & Read First Part
+            temp_message = message.split('\r\n')
+            data = numpy.fromstring(temp_message[0], dtype = numpy.float, sep=',')[2 : 2 + self.channels]
+            
+            if data.shape[0] == self.channels:
+                
+                # Save Data
+                signal = numpy.append(signal, data.reshape(1, self.channels), axis = 0)
+                stimulus_code = numpy.append(stimulus_code, self.gui.stimulus_code + 1)
+                
+                message = ''
+                if len(temp_message) > 1:
+                    for index in numpy.arange(1, len(temp_message)):
+                        message += temp_message[index]
+                
+                message_counter += 1
+                if message_counter == self.end_samples_count:
+                    break
         
         return signal, stimulus_code
     
@@ -381,12 +422,12 @@ class P300_Controller(object):
 #from emotivsocket import EmotivSocketSender
 #_thread.start_new_thread(EmotivSocketSender, ())
 
-cntrlr = P300_Controller(CHARACTER_MATRIX, font = 'Courier 70', repetitions=5)
+cntrlr = P300_Controller(CHARACTER_MATRIX, font = 'Courier 70', repetitions=3, state_0_delay=0.11, state_1_delay=0.076)
 
-target_char = 'A'
+target_char = 'AJSWO185'
 target_char = ''.join(random.sample(target_char, len(target_char)))
 
-signal, stimulus_code = cntrlr.train_session(target_char, plot = True)
+signal, stimulus_code = cntrlr.train_session(target_char, plot=True)
 for index in range(len(target_char)):
     numpy.savetxt('signal_' + target_char[index] + '.txt', signal[index])
     numpy.savetxt('stimulus_code_' + target_char[index] + '.txt', stimulus_code[index])
